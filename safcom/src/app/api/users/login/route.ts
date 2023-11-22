7cd142d69bac44749e135933a146ec9d24c9f9f9
Ruth Moraa <moraar37@gmail.com>
2023-11-22 05:07:56 +0300

    login page and navigation, debugging

---
+++ safcom/src/app/api/users/login/route.ts
@@ -0,0 +1,62 @@
+import bcryptjs from 'bcryptjs';
+import {
+  NextRequest,
+  NextResponse,
+} from 'next/server';
+import { connect } from "src/dbConfig/dbConfig"
+import User from '@/src/models/userModel';
+import jwt from 'jsonwebtoken';
+
+
+
+connect()
+
+export async function POST (request: NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {email, password} = reqBody
+
+        console.log(reqBody)
+
+        //CHECK IF USER EXISTS
+        const user = await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error: "User does not exists"}, {status: 400})
+
+        }
+        console.log('user exists'); 'user exists'
+        // check if password exists
+        const validPassword = await bcryptjs.compare
+        (password, user.password)
+        if(!validPassword){
+            return NextResponse.json({
+                error: "Invalid Password",
+                              
+            }, {status: 400})
+
+        }
+        // create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+        //create token
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!,
+            {expiresIn: "1d"})
+        
+        const response = NextResponse ({
+            message:'LOGIN SUCCESSFUL',
+            success: true
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true, 
+        })
+        return response;
+
+    } catch(error: any){
+        return NextResponse.json({error: error.message},
+        {status: 500})
+
+    }
+}
--- safcom/src/app/login/page.tsx
+++ safcom/src/app/login/page.tsx
@@ -1,26 +1,58 @@
 
 "use client";
 import Link from "next/link";
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios  from "axios";
 
 
 
 
 export default function LoginPage(){ 
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         
     })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false)
+
 
     const onLogin = async () => {
+        try {
+            setLoading(true);
+        
+            router.push("/profile");
+             const response = await axios.post("api/users/login", user)
+             
+          
+              console.log("Login success", response.data);
+            //  router.push("/profile");
+           
+
+            
+        } catch (error: any) {
+            console.log("login failed", error.message);
+            // toast.error(error.message)
+            
+        } finally {
+            setLoading(false)
+        }
 
     }
+
+    useEffect (() => {
+        if(user.email.length > 0 && user.password.length > 0){
+            setButtonDisabled(false)
+
+        } else {
+            setButtonDisabled(true)
+        }
+    }, [user])
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-            <h1>Login</h1>
+            <h1>{loading ? "Processing" : "Logged in"}</h1>
             <hr />
          <hr/>
              <label htmlFor="email">email</label><hr/>
