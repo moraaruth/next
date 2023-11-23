f8592b47350a3828f9ddd3f5fab0bcd48c45a096
Ruth Moraa <moraar37@gmail.com>
2023-11-23 04:59:18 +0300

    adding middleware and me route

---
+++ safcom/src/app/api/users/me/route.ts
@@ -0,0 +1,26 @@
+import { getDataFromToken } from "@/src/helpers/fetDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from '@/src/models/userModel';
+import { connect } from "@/src/dbConfig/dbConfig"
+
+
+connect()
+
+export async function GET(request: NextRequest){
+    try {
+       const userId = await getDataFromToken(request)
+       const user = await User.findOne({_id: userId}).
+       select("-password");
+       return NextResponse.json({
+           message: "User found",
+           data: user
+       })
+        
+    } catch (error:any) {
+        return NextResponse.json({
+            error: error.message
+        }, {status: 400})
+        
+    }
+
+}
--- safcom/src/app/profile/page.tsx
+++ safcom/src/app/profile/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import { useState } from 'react'
 import { useRouter } from "next/navigation"
 import axios from "axios"
 import Link from "next/link"
 
 export default function ProfilePage(){
     const router = useRouter()
+    const [data, setData] = useState("")
 
     const logout =  async () => {
         try {
@@ -17,17 +19,37 @@ export default function ProfilePage(){
         }
 
     }
+
+    const getUserDetails = async () => {
+       const res = await axios.get('api/users/me')
+       console.log(res.data);
+       setData(res.data.data._id)
+    }
     return (
         <div>
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2>{data === 'nothing'? "Nothing" : 
+            <Link
+            href={`/profile/${data}`}
+            >
+                {data}
+
+            </Link>}
+            </h2>
             <hr />
             <button 
             onClick={logout}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
                 Logout
+            </button>
+            <button 
+            onClick={getUserDetails}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                User Details 
             </button>
         </div>
     )
